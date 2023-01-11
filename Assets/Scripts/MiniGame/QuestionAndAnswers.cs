53c6622a530d93b2523ac325e120ebbb7b1b88b0
Joshua <allenwalker0853@yahoo.com>
2023-01-11 16:51:39 +0800

    Assets
    asset folder, mini game

---
+++ Assets/Scripts/MiniGame/QuestionAndAnswers.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class QuestionAndAnswers
+{
+    public Sprite Question;
+    public string[] Answers;
+    public int CorrectAnswer;
+}
