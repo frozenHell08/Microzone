39c5fbfaa2dcc6fc614ebbf2dd1f6394341bcbe1
Joshua <frozen.hell08@gmail.com>
2023-05-27 22:11:56 +0800

    Characters
    - movement
    - collider
    - map 1-4 drop chara & enemy

---
+++ Assets/Characters/Movementz.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movementz : MonoBehaviour
+{
+    Rigidbody rb;
+
+    // Player
+    // float speedLimiter = 50f;
+    [SerializeField] private float speedLimiter;
+    float inputHorizontal;
+    float inputVertical;
+
+    // Animations and states
+    Animator animator;
+    string currentState;
+    const string F_idle = "F_idle";
+    const string F_walk_left = "F_walk_left";
+    const string F_walk_right = "F_walk_right";
+    const string F_walk_down = "F_walk_down";
+    const string F_walk_up = "F_walk_up";
+
+    private LevelLogic _levelLogic;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        animator = gameObject.GetComponent<Animator>();
+        _levelLogic = GameObject.Find("GameController").GetComponent<LevelLogic>();
+    }
+
+
+    void Update()
+    {
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+        inputVertical = Input.GetAxisRaw("Vertical");
+    }
+    void FixedUpdate()
+    {
+        if (inputHorizontal != 0 || inputVertical != 0)
+        {
+            if (inputHorizontal != 0 && inputVertical != 0)
+            {
+                inputHorizontal *= speedLimiter;
+                inputVertical *= speedLimiter;
+            }
+            rb.velocity = new Vector3(inputHorizontal, 0, inputVertical);
+
+            if (inputHorizontal > 0)
+            {
+                ChangeAnimationState(F_walk_right);
+            }
+
+            else if (inputHorizontal < 0)
+            {
+                ChangeAnimationState(F_walk_left);
+            }
+
+            else if (inputVertical > 0)
+            {
+                ChangeAnimationState(F_walk_up);
+            }
+
+            else if (inputVertical < 0)
+            {
+                ChangeAnimationState(F_walk_down);
+            }
+        }
+        else
+        {
+            rb.velocity = new Vector3(0f, 0f);
+            ChangeAnimationState(F_idle);
+        }
+    }
+
+    // Animation state changer
+    void ChangeAnimationState(string newState)
+    {
+        // Stop animation interrupting itself
+        if (currentState == newState) return;
+
+        // Play new animation
+        animator.Play(newState);
+
+        // Update current state
+        currentState = newState;
+    }
+
+    void OnCollisionEnter (Collision col) {
+        if (col.gameObject.tag == "Enemy") {
+            Destroy(col.gameObject);
+            Debug.Log("boop");
+            _levelLogic.AddKill();
+        } else if (col.gameObject.tag == "Player") {
+            Debug.Log("MINUS");
+        }
+    }
+}
---
+++ Assets/Characters/PlayerControler4.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler4 : MonoBehaviour
+{
+    public Rigidbody rb;
+    private Animator myAnim;
+
+    private float speed = 0f;
+    // [SerializeField] private int speed;
+
+    private float attackTime = 1;
+    private float attackCounter = 1f;
+    private bool IsAttacking;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        myAnim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        rb.velocity = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * speed * Time.deltaTime;
+        myAnim.SetFloat("moveX", rb.velocity.x);
+        myAnim.SetFloat("moveY", rb.velocity.y);
+
+        if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        {
+            myAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+            myAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+        }
+
+        if (IsAttacking)
+        {
+            rb.velocity = Vector3.zero;
+            attackCounter -= Time.deltaTime;
+            if(attackCounter <= 0)
+            {
+                myAnim.SetBool("IsAttacking", false);
+                IsAttacking = false;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            attackCounter = attackTime;
+            myAnim.SetBool("IsAttacking", true);
+            IsAttacking = true;
+        }
+    }
+}
